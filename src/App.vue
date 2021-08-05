67b7f909558b0f90cf70a2a6e70747da79b4b478
junseinagao <junsei.nagao@gmail.com>
2021-08-05 20:17:17 +0900

    変更したよ

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div>templateでは、HTMLとVue構文を書く。</div>
     <div>{{ hello }}</div>
+    <div>変更しました</div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
